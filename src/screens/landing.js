ca4a2f3a83f2b247694b2641280a7714169ac1c6
Haniel Diaz <gilastomper@gmail.com>
2018-11-13 21:37:37 -0500

    update node Modules

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+
+
+class About extends Component {
+
+  render() {
+    return (
+        <span>
+            hello
+        </span>
+    );
+  };
+}
+
+export { About };
